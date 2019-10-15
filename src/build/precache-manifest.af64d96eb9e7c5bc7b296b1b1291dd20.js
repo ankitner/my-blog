b87d591827d550aa6289124977e2d4415d6264e7
ankitner <ankitner@buffalo.edu>
2019-10-14 21:30:33 -0400

    Initial Commit

---
+++ src/build/precache-manifest.af64d96eb9e7c5bc7b296b1b1291dd20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3ef0e84453fbd75fce48",
+    "url": "/static/js/main.c68d2b0c.chunk.js"
+  },
+  {
+    "revision": "2994cd57851931a2cafc",
+    "url": "/static/js/2.0a041210.chunk.js"
+  },
+  {
+    "revision": "3ef0e84453fbd75fce48",
+    "url": "/static/css/main.c79c6f40.chunk.css"
+  },
+  {
+    "revision": "ba49a058cf264e36b32bde4ecee8f688",
+    "url": "/index.html"
+  }
+];
